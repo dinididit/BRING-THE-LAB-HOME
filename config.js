c7b5a90d9013f36e433168adc49e3ca93763431d
dinididit <35192425+dinididit@users.noreply.github.com>
2026-09-11 01:24:18 -0400

    Add campaign configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.BRING_THE_LAB_HOME = {
+  fundraiserUrl: "",
+  repositoryUrl: "https://github.com/dinididit/BRING-THE-LAB-HOME",
+  publicUrl: "https://dinididit.github.io/BRING-THE-LAB-HOME/"
+};
